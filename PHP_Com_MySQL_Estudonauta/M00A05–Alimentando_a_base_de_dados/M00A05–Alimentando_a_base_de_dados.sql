aeb2b8a03b2f2bebaf370d3f43293e9f0ba648f7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-07 16:53:25 +0100

    M00A05 – Alimentando a base de dados

--- PHP_Com_MySQL_Estudonauta/M00A05–Alimentando_a_base_de_dados/M00A05–Alimentando_a_base_de_dados.sql
+++ PHP_Com_MySQL_Estudonauta/M00A05–Alimentando_a_base_de_dados/M00A05–Alimentando_a_base_de_dados.sql
@@ -3,4 +3,13 @@ use db_games;
 insert into generos values
 (1, 'Ação'),
 (2, 'Aventura'),
-(3, 'Terror')
+(3, 'Terror'),
+(4, 'Plataforma'),
+(5, 'Estratégia'),
+(6, 'RPG'),
+(7, 'Esporte'),
+(8, 'Corrida'),
+(9, 'Tabuleiro'),
+(10, 'Puzzle'),
+(11, 'Luta'),
+(12, 'Musical');
