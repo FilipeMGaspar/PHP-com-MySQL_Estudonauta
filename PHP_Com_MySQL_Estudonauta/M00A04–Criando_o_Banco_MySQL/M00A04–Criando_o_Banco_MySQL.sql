28e81eb0c912779a0e3e403fac6174b9eaefcd43
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-07 16:27:06 +0100

    M00A04 – Criando o Banco MySQL

--- PHP_Com_MySQL_Estudonauta/M00A04–Criando_o_Banco_MySQL/M00A04–Criando_o_Banco_MySQL.sql
+++ PHP_Com_MySQL_Estudonauta/M00A04–Criando_o_Banco_MySQL/M00A04–Criando_o_Banco_MySQL.sql
@@ -2,8 +2,9 @@ create database db_games default character set utf8 collate utf8_general_ci;
 use db_games;
 
 create table usuarios (
-	usuario varchar(10) not null auto_increment,
+	usuario varchar(10) not null,
     nome varchar(30) not null,
     senha varchar(60) not null,
-    tipo varchar(10) not null default 'editor'
+    tipo varchar(10) not null default 'editor',
+    primary key(usuario)
 ) engine=InnoDB default charset=utf8;
