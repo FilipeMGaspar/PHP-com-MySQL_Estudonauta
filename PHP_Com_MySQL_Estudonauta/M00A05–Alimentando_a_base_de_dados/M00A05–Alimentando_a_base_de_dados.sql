31541b6ed69b9f0a14a2ca56479b5ec10975a0ee
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-07 17:12:24 +0100

    M00A05 – Alimentando a base de dados

--- PHP_Com_MySQL_Estudonauta/M00A05–Alimentando_a_base_de_dados/M00A05–Alimentando_a_base_de_dados.sql
+++ PHP_Com_MySQL_Estudonauta/M00A05–Alimentando_a_base_de_dados/M00A05–Alimentando_a_base_de_dados.sql
@@ -13,3 +13,21 @@ insert into generos values
 (10, 'Puzzle'),
 (11, 'Luta'),
 (12, 'Musical');
+
+select * from generos;
+
+insert into produtoras (cod, produtora, pais) values
+(1, 'Microsoft', 'EUA'),
+(2, 'Tencent', 'China'),
+(3, 'Nintendo', 'Japão'),
+(4, 'Sony', 'Japão'),
+(5, 'Activision', 'EUA'),
+(6, 'EA', 'EUA'),
+(7, 'Sega', 'Japão'),
+(8, 'Nanco Bandai', 'Japão'),
+(9, 'Konami', 'Japão'),
+(10, 'Ubisoft', 'EUA'),
+(11, 'Valve', 'EUA'),
+(12, 'Square Enix', 'Japão'),
+(13, 'Take Two', 'EUA'),
+(14, 'Capcom', 'Japão');
