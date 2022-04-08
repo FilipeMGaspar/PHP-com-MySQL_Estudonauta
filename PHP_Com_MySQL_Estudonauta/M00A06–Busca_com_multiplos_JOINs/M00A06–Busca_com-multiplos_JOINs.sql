8b3f9d5f879854860d230f853923c5461f7eca57
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-08 10:33:33 +0100

    M00A06 – Busca com múltiplos JOINs

--- PHP_Com_MySQL_Estudonauta/M00A06–Busca_com_multiplos_JOINs/M00A06–Busca_com-multiplos_JOINs.sql
+++ PHP_Com_MySQL_Estudonauta/M00A06–Busca_com_multiplos_JOINs/M00A06–Busca_com-multiplos_JOINs.sql
@@ -3,3 +3,5 @@ use bd_games;
 SELECT * FROM `jogos` JOIN `generos`  ON jogos.genero = generos.cod;
 
 SELECT jogos.cod, nome, generos.genero, descricao, nota, capa FROM `jogos` JOIN `generos`  ON jogos.genero = generos.cod;
+
+SELECT jogos.cod, nome, generos.genero, produtoras.produtora, descricao, nota, capa FROM `jogos` JOIN `generos`  ON jogos.genero = generos.cod JOIN produtoras ON jogos.produtora = produtoras.cod
