302b887f53bbb9d014674c77ec4e8f50f4934e78
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-10 17:04:47 +0100

    Exercício - M01A02 – Integrando dados ao HTML criado

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/includes/banco.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/includes/banco.php
@@ -2,7 +2,25 @@
     //$banco = new mysqli(host, utilizador, senha, nome da base de dados);
     $ligacaoBanco = new mysqli("localhost", "root", "", "db_vendeBolachas");
 
-    if($banco->connect_errno) {
+    if($ligacaoBanco->connect_errno) {
         echo "<p>Encontreio um erro $banco->connect_errno --> $banco->connect_error</p>";
         die();//Mata o processo de conecção com a base de dadao se der algum erro
     }
+
+        //Query ao banco de dados para codificação de dados em utf8
+        $banco->query("SET NAMES 'UTF8'");
+        $banco->query("SET character_set_connection=utf8");
+        $banco->query("SET character_set_client=utf8");
+        $banco->query("SET character_set_results=utf8");
+/*    
+        //Consulta com select
+        $busca = $banco->query("select * from generos");
+        if(!$busca){
+            echo "</p>Falha na busca! $banco->error</p>";
+        } else {
+            
+          while($registo = $busca->fetch_object()){
+            print_r($registo);
+          }
+        }
+*/
