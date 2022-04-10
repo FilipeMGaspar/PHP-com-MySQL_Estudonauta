b7c569a8c5d3736687022b630389b7dd136fedf1
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-10 17:03:47 +0100

    Exercício - M01A02 – Integrando dados ao HTML criado

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/includes/banco.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/includes/banco.php
@@ -1,3 +1,8 @@
 <?php
     //$banco = new mysqli(host, utilizador, senha, nome da base de dados);
     $ligacaoBanco = new mysqli("localhost", "root", "", "db_vendeBolachas");
+
+    if($banco->connect_errno) {
+        echo "<p>Encontreio um erro $banco->connect_errno --> $banco->connect_error</p>";
+        die();//Mata o processo de conecção com a base de dadao se der algum erro
+    }
