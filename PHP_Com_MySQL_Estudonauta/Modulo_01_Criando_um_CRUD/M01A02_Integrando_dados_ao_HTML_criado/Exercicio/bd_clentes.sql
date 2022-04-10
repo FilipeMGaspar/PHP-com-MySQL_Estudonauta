c3c5de5996d0d4b60602c48f78d6986ecbe7bc12
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-10 16:22:53 +0100

    Exercício - M01A02 – Integrando dados ao HTML criado

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/bd_clentes.sql
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A02_Integrando_dados_ao_HTML_criado/Exercicio/bd_clentes.sql
@@ -2,4 +2,10 @@ CREATE DATABASE db_vendeBolachas;
 
 USE db_vendeBolachas;
 
-create table clientes;
+create table clientes(
+	NrCliente int not null auto_increment,
+    NomeCliente varchar(40) not null,
+    Endereço varchar(120) not null,
+    telefone int(9) not null,
+    primary key(NrCliente)
+) engine=InnoDB default charset=utf8;
