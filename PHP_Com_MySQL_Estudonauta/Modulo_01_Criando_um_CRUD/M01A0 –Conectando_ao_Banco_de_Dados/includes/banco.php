cb4f1f60e794d4a3652cd921ef154251cd08ca83
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-10 11:07:42 +0100

    M01A01 – Conectando ao Banco de Dados

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A0 –Conectando_ao_Banco_de_Dados/includes/banco.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A0 –Conectando_ao_Banco_de_Dados/includes/banco.php
@@ -1,6 +1,7 @@
 <?php
     //$banco = new mysqli(host, utilizador, senha, nome da base de dados);
-    $banco = new mysqli("localhost", "root", "", "bd_games");
+    $banco = new mysqli("locahost", "root", "", "bd_games");
     if($banco->connect_errno) {
         echo "<p>Encontreio um erro $banco->connect_errno --> $banco->connect_error</p>";
+        die();//Mata o processo de conecção com a base de dadao se der algum erro
     }
