75e85f4ce527e884ee2a6f27e6d7f6540805d96f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-18 13:27:38 +0100

    M01A03 – Imagens no Banco de Dados

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
@@ -24,7 +24,7 @@
                         echo "<tr><td>Nenhum registo encontrado!";
                     } else {
                         while($reg = $busca->fetch_object()){
-                            echo "<tr><td><img src = 'fotos/$reg->capa' id = 'mini'/><td>$reg->nome";
+                            echo "<tr><td><img src = 'fotos/$reg->capa' class = 'mini'/><td>$reg->nome";
                             echo "<td>Adm";
                         }
                     }
