76ff0bd78566389166593e7bae5e8ff5e3f97556
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-18 14:17:56 +0100

    M01A03 – Imagens no Banco de Dados

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
@@ -9,6 +9,9 @@
     <?php
         function thumb($arq){
             $caminho = "fotos/$arq";
+            if(is_null($arq) || !file_exists($caminho)){
+                
+            }
         }
     ?>
 </head>
