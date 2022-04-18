4ceaef11d858d2eb22b4e0779a049823425e8c20
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-18 14:24:07 +0100

    M01A03 – Imagens no Banco de Dados

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
@@ -6,16 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo/estilo.css">
     <title>Listagem de Jogos</title>
-    <?php
-        function thumb($arq){
-            $caminho = "fotos/$arq";
-            if(is_null($arq) || !file_exists($caminho)){ //Se for nulo ou o arquivo não existir
-                return "fotos/indisponivel.png"
-            } else {
-                return $caminho;
-            }
-        }
-    ?>
 </head>
 <body>
     <?php
