23e0f62fa6490bbc4011e1f4d97d7406a5a0ae29
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-18 14:15:49 +0100

    M01A03 – Imagens no Banco de Dados

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A03_Imagens_no_Banco_de_Dados/M01A03.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo/estilo.css">
     <title>Listagem de Jogos</title>
+    <?php
+        function thumb($arq){
+            $caminho = "fotos/$arq";
+        }
+    ?>
 </head>
 <body>
     <?php
