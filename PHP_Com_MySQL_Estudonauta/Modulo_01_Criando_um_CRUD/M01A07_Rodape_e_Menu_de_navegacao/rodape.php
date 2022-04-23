324d3a8986659852f3b13f6e024fe56250184e02
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-23 15:30:24 +0100

    M01A07 – Rodapé e Menu de navegação

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A07_Rodape_e_Menu_de_navegacao/rodape.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A07_Rodape_e_Menu_de_navegacao/rodape.php
@@ -1,5 +1,5 @@
 <?php
     echo "<footer>";
     echo "<p>Acessado por ... em ... </p>";
-    echo "<p>Desenvolvido por Estudonauta &copy; 2017</p>";
+    echo "<p>Desenvolvido por Estudonauta &copy; 2022</p>";
     echo "</footer>";
