d28679fa70f19c82e6f78b1fb29b1b792c50ae0c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-23 15:32:09 +0100

    M01A07 – Rodapé e Menu de navegação

--- PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A07_Rodape_e_Menu_de_navegacao/rodape.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A07_Rodape_e_Menu_de_navegacao/rodape.php
@@ -1,5 +1,8 @@
 <?php
+require_once "includes/banco.php";
     echo "<footer>";
     echo "<p>Acessado por ... em ... </p>";
     echo "<p>Desenvolvido por Estudonauta &copy; 2022</p>";
     echo "</footer>";
+
+    $banco->close(); //Fecho da ligação á base de dados
