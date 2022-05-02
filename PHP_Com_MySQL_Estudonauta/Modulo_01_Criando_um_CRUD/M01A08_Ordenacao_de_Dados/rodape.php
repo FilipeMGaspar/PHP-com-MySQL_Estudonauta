4e5ed4ce0556681baeb538f5017d5fd89effcf08
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-02 09:33:08 +0100

    M01A08 – Ordenação de Dados

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_01_Criando_um_CRUD/M01A08_Ordenacao_de_Dados/rodape.php
@@ -0,0 +1,8 @@
+<?php
+require_once "includes/banco.php";
+    echo "<footer>";
+    echo "<p>Acessado por " . $_SERVER['REMOTE_ADDR'] . " em " . date('d/m/Y') . "</p>";
+    echo "<p>Desenvolvido por Estudonauta &copy; 2022</p>";
+    echo "</footer>";
+
+    $banco->close(); //Fecho da ligação á base de dados
