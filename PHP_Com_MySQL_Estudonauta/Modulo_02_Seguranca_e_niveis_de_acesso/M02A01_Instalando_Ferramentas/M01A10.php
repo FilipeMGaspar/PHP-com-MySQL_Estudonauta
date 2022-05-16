cb32bce1fb739bdd8b0a1fee03e8c7b1404c63c9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-16 14:06:56 +0100

    M02A01 – Instalando Ferramentas

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A01_Instalando_Ferramentas/M01A10.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <title>Listagem de Jogos</title>
+</head>
+<body>
+    <?php
+        require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
+        $ordem = $_GET['o'] ?? "n";
+        $chave =$_GET['c'] ?? "";
+    ?>
+    <div id="corpo">
+        <?php include_once "topo.php"?>
+        <h1>Selecione um jogo</h1>
+        <form method="get" id="busca" action="">
+            Ordenar: 
+            <a href="M01A10.php?o=n&c=<?php echo $chave;?>">Nome</a> | 
+            <a href="M01A10.php?o=p&c=<?php echo $chave;?>">Produtora</a> | 
+            <a href="M01A10.php?o=na<?php echo $chave;?>">Nota Alta</a> |
+            <a href="M01A10.php?o=nb<?php echo $chave;?>">Nota Baixa</a> |
+            <a href="M01A10.php">Mostrar Todos</a> |
+            Buscar: <input type="text" name="c" size="10" maxlength="40">
+            <input type="submit" value="Ok">
+        </form>
+        <table class="listagem">
+            <?php
+                $q = "SELECT jogos.cod, nome, generos.genero, produtoras.produtora, descricao, nota, capa FROM `jogos` JOIN `generos`  ON jogos.genero = generos.cod JOIN produtoras ON jogos.produtora = produtoras.cod ";
+
+                if(!empty($chave)){//Se a chave não estiver vazia
+                    $q .= "WHERE jogos.nome like '%$chave%' OR produtoras.produtora like '%$chave%' OR generos.genero like '%$chave%' ";
+                }
+
+                switch($ordem){
+                    case "p":
+                        $q .= "ORDER BY produtoras.produtora";
+                        break;
+                    case "na": 
+                        $q .= "ORDER BY jogos.nota DESC"; // ordem decrescente
+                        break; 
+                    case "nb":
+                        $q .= "ORDER BY jogos.nota ASC";  // ordem crescente
+                        break;
+                    default:
+                        $q .= "ORDER BY jogos.nome";
+                        break;
+
+                }
+                $busca = $banco->query($q);
+                if(!$busca){//Verifica se a busca não aconteceu apresentando um erro
+                    echo "<tr><td>Infelizmente não foi possivél efectuar a busca!";
+                } else {
+                    if($busca->num_rows == 0){
+                        echo "<tr><td>Nenhum registo encontrado!";
+                    } else {
+                        while($reg = $busca->fetch_object()){
+                            $imgThumb = thumb($reg->capa);  
+                            echo "<tr><td><img src='$imgThumb' class='mini'/>";
+                            echo "<td><a href='detalhes.php?cod=$reg->cod'>$reg->nome</a>";
+                            echo " [$reg->genero]";
+                            echo "<br> $reg->produtora";
+                            echo "<td>Adm";
+                        }
+                    }
+                }
+            ?>
+        </table>
+    </div>
+    <?php include_once "rodape.php";?>
+</body>
+</html>
