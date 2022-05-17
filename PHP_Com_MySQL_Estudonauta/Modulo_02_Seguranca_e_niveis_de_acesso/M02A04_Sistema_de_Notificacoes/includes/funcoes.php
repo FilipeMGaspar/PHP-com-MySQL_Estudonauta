b1363df27ca45d914a592687fff4840aeba325f8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-17 09:29:35 +0100

    M02A04 – Sistema de Notificações

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A04_Sistema_de_Notificacoes/includes/funcoes.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A04_Sistema_de_Notificacoes/includes/funcoes.php
@@ -12,7 +12,16 @@
             return "<span class='material-symbols-outlined'>keyboard_double_arrow_left</span>";
         }
 
-        function msg_sucesso() {
+        function msg_sucesso($msg) {
             
         }
+
+        function msg_aviso ($msg){
+
+        }
+
+        function msg_erro($msg) {
+
+        }
+
     ?>
