d9169db17399088af70403e4b0722b6573eff751
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-19 10:54:12 +0100

    M02A06 – Criptografia simples

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A06_Criptografia_simples/includes/login.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A06_Criptografia_simples/includes/login.php
@@ -17,13 +17,4 @@
         return $ok;
     }
 
-    /*
-    echo gerarHash('Teste') . "<br>";
-
-    $tstHash = '$2y$10$hJjCG9/iuf8MR/qtpBi/te8qB67z02ZNjXQr2mNdcqRBtN93CBIhy';
-    if(testarHash('teste', $tstHash)) {
-        echo "Senha Correcta <br>";
-    } else {
-        echo "Senha Inválida! <br>";
-    }
-    */
+    
