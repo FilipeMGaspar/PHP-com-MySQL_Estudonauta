72ee6fdca872e4697fd42f51bce041ed4be839fc
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-24 19:10:36 +0100

    M02A07 – Tela de login

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/detalhes.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/detalhes.php
@@ -14,6 +14,7 @@
     <?php
         require_once "includes/banco.php";
         require_once "includes/funcoes.php";
+        require_once "includes/login.php";
     ?>
     <div id="corpo">
         <?php
