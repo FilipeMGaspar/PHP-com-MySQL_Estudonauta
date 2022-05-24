614d04da7f2cbf81ecb78e7e46f6708c8693453f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-24 19:53:46 +0100

    M02A07 – Tela de login

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/user-login-form.php
@@ -0,0 +1,7 @@
+<form action="user-login.php" method="post">
+    <table>
+        <tr><td>Utilizador:<td><input type="text" name="usuario" id="usuario">
+        <tr><td>Utilizador:<td><input type="password" name="senha" id="senha">
+        <tr><td><input type="submit">
+    </table>
+</form>
