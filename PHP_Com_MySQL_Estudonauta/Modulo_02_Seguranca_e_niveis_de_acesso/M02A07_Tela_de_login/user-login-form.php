57b2069f857893d38c4979cc4266a3200e614664
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-24 19:57:50 +0100

    M02A07 – Tela de login

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/user-login-form.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/user-login-form.php
@@ -1,8 +1,8 @@
 <h1>Efectue Login:</h1>
 <form action="user-login.php" method="post">
     <table>
-        <tr><td>Utilizador:<td><input type="text" name="usuario" id="usuario">
-        <tr><td>Utilizador:<td><input type="password" name="senha" id="senha">
+        <tr><td>Utilizador:<td><input type="text" name="usuario" id="usuario" size="10" maxlength="10">
+        <tr><td>Utilizador:<td><input type="password" name="senha" id="senha" size="10" maxlength="10">
         <tr><td><input type="submit">
     </table>
 </form>
