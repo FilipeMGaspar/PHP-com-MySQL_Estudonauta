efb8d5948bdd44fec4ae42ddd9999308b143d1b9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-24 19:34:40 +0100

    M02A07 – Tela de login

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/topo.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/topo.php
@@ -1,7 +1,7 @@
 <?php
     echo "<header>";
     if(empty($_SESSION['user'])){
-        echo  "<a href='user-login.php'>Entrar</a>"; 
+        echo  "<a href='user-login.php'>Entrar</a>" .$_SESSION['user']; 
     } else {
         echo "Olá, " . $_SESSION['nome'];
     }    
