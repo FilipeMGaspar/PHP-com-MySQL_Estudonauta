3fded952d3db762a57f804e8f297dc1bcb375e8e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-24 19:15:31 +0100

    M02A07 – Tela de login

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A07_Tela_de_login/__Modelo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <!-- Icons do Google-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Título da página</title>
+</head>
+<body>
+    <?php
+        require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
+        require_once "includes/login.php";
+    ?>
+    <div id="corpo">
+    </div>
+</body>
+</html>
