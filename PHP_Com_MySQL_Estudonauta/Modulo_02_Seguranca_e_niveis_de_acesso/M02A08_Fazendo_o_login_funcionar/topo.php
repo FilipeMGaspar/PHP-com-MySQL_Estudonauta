92e82751db320e683b304b97c049476425f06b2e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-25 10:08:54 +0100

    M02A08 – Fazendo o login funcionar

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A08_Fazendo_o_login_funcionar/topo.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A08_Fazendo_o_login_funcionar/topo.php
@@ -3,6 +3,6 @@
     if(empty($_SESSION['user'])){
         echo  "<a href='user-login.php'>Entrar</a>"; 
     } else {
-        echo "Olá, " . $_SESSION['nome'];
+        echo "Olá, <strong>" . $_SESSION['nome'] . "</strong>";
     }    
     echo "</header>";
