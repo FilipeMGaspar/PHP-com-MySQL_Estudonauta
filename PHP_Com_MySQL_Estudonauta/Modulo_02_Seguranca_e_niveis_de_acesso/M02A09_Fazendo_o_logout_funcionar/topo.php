e0a97a8004a3d6363fb210fc9515f6113221dddf
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-26 22:12:43 +0100

    M02A09 – Fazendo o logout funcionar

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A09_Fazendo_o_logout_funcionar/topo.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<header>";
+    if(empty($_SESSION['user'])){
+        echo  "<a href='user-login.php'>Entrar</a>"; 
+    } else {
+        echo "Olá, <strong>" . $_SESSION['nome'] . "</strong>  | ";
+        echo "Sair";
+    }    
+    echo "</header>";
