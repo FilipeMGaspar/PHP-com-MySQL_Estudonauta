b17eacca40dc578975bed18a748e55029f086bc3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-30 09:34:22 +0100

    M02A10 – Níveis de Acesso

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A10_Niveis_de_Acesso/topo.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A10_Niveis_de_Acesso/topo.php
@@ -4,6 +4,11 @@
         echo  "<a href='user-login.php'>Entrar</a>"; 
     } else {
         echo "Olá, <strong>" . $_SESSION['nome'] . "</strong>  | ";
+        echo " Meus Dados | ";
+        if(is_admin()){
+            echo " Novo utilizador |";
+            echo " Novo jogo |"
+        }
         echo "<a href='user-logout.php'>Sair</a>";
     }    
     
