8f0bb0b20922038f075e2ea6773f1a4eb3085ef8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-31 12:34:44 +0100

    M02A11 – Formulário novo usuário

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="estilo/estilo.css">
     <!-- Icons do Google-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <title>???</title>
+    <title>Gestão de Utilizadores</title>
 </head>
 <body>
     <?php
@@ -17,7 +17,9 @@
         require_once "includes/login.php";
     ?>
     <div id="corpo">
-
+        <?php
+           
+        ?>
     </div>
 </body>
 </html>
