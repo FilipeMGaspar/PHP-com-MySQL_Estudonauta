a86c621177d38bb008049e2361a5a7bdbab85712
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-31 12:45:15 +0100

    M02A11 – Formulário novo usuário

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new.php
@@ -19,10 +19,20 @@
     <div id="corpo">
         <?php
            if(!is_admin()){
-                echo msg_erro("Área restrita! Não é Administrador!");
+                echo msg_erro("Área restrita! Não é administrador!");
                 echo voltar();
            } else {
-               echo "<h1>Novo Utilizador</h1>";
+                if(!isset($_POST['usuario'])){
+                    require "user-new-form.php";
+                } else {
+                    $usuario = $_POST['usuario'] ?? null;
+                    $nome = $_POST['nome'] ?? null;
+                    $senha1 = $_POST['senha1'] ?? null;
+                    $senha2 = $_POST['senha2'] ?? null;
+                    $tipo = $_POST['tipo'] ??  null
+                }
+                echo "<h1>Novo Utilizador</h1>";
+
            }
         ?>
     </div>
