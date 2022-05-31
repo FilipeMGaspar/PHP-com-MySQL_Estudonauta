9682cd06320cc826ce900fb5e6e73275731006c2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-31 12:53:09 +0100

    M02A11 – Formulário novo usuário

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new-form.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A11_Formulario_novo_usuario/user-new-form.php
@@ -1,2 +1,12 @@
 <h1>Novo Utilizador</h1>
-<form action="" method="post"></form>
+<form action="user-new.php" method="post">
+    <table>
+        <tr><td>Utilizador</td><td><input type="text" name="usuario" id="usuario" size="10" maxlength="10"></td></tr>
+        <tr><td>Nome</td><td><input type="text" name="nome" id="nome" size="30" maxlength="30"></td></tr>
+        <tr><td>Tipo</td><td><select name="tipo" id="tipo">
+            <option value="admin"> Administrador</option>
+            <option value="editor"> Editor</option>
+        </select></td></tr>
+    </table>
+
+</form>
