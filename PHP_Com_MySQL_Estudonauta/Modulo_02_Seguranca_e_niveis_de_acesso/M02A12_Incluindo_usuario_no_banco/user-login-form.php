fc418308199c6fac4d8fdd2cd77ebb7acf1f2302
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-01 09:31:46 +0100

    M02A12 – Incluindo usuário no banco

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A12_Incluindo_usuario_no_banco/user-login-form.php
@@ -0,0 +1,8 @@
+<h2>Efectue Login:</h2>
+<form action="user-login.php" method="post">
+    <table>
+        <tr><td>Utilizador:<td><input type="text" name="usuario" id="usuario" size="10" maxlength="10">
+        <tr><td>Password:<td><input type="password" name="senha" id="senha" size="10" maxlength="10">
+        <tr><td><input type="submit">
+    </table>
+</form>
---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A12_Incluindo_usuario_no_banco/user-new.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <!-- Icons do Google-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Gestão de Utilizadores</title>
+</head>
+<body>
+    <?php
+        require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
+        require_once "includes/login.php";
+    ?>
+    <div id="corpo">
+        <?php
+           if(!is_admin()){
+                echo msg_erro("Área restrita! Não é administrador!");
+                echo voltar();
+           } else {
+                if(!isset($_POST['usuario'])){
+                    require "user-new-form.php";
+                } else {
+                    $usuario = $_POST['usuario'] ?? null;
+                    $nome = $_POST['nome'] ?? null;
+                    $senha1 = $_POST['senha1'] ?? null;
+                    $senha2 = $_POST['senha2'] ?? null;
+                    $tipo = $_POST['tipo'] ??  null;
+
+                    if($senha1 === $senha2){
+                        echo msg_sucesso("Tudo certo para gravar!");
+                    } else {
+                        echo msg_erro("As Passwords devem ser coincidir!") . "<br>";
+                        echo msg_erro("Por favor, repita o procedimento!");
+                    }
+                }
+           }
+           echo voltar();
+        ?>
+    </div>
+</body>
+</html>
