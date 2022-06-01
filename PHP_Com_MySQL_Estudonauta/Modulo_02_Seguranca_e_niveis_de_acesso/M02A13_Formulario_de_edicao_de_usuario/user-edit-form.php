0d4ea8e6c9b8ef06f78a1699b7d5ce4464930d13
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-01 10:09:31 +0100

    M02A13 – Formulário de edição de usuário

--- PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A13_Formulario_de_edicao_de_usuario/user-edit-form.php
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A13_Formulario_de_edicao_de_usuario/user-edit-form.php
@@ -1,1 +1,8 @@
 <h1>Alteração de dados</h1>
+<form action="user-edit.php" method="post">
+    <table>
+        <tr><td>Utilizador</td><td><input type="text" name="usuario" id="usuario" size="10" maxlength="10"></td></tr>
+        <tr><td>Nome</td><td><input type="text" name="nome" id="nome" size="30" maxlength="30"></td></tr>
+        <tr><td>Tipo</td><td><input type="text" name="tipo" id="tipo" readonly></td></tr>
+    </table>
+</form>
