acd9791d812d3e887eaa107270cfd802bd0a962c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-02 14:29:26 +0100

    M02A14 – Alterando dados do Banco

---
+++ PHP_Com_MySQL_Estudonauta/Modulo_02_Seguranca_e_niveis_de_acesso/M02A14_Alterando_dados_do_Banco/user-edit-form.php
@@ -0,0 +1,11 @@
+<h1>Alteração de dados</h1>
+<form action="user-edit.php" method="post">
+    <table>
+        <tr><td>Utilizador</td><td><input type="text" name="usuario" id="usuario" size="10" maxlength="10"></td></tr>
+        <tr><td>Nome</td><td><input type="text" name="nome" id="nome" size="30" maxlength="30"></td></tr>
+        <tr><td>Tipo</td><td><input type="text" name="tipo" id="tipo" readonly></td></tr>
+        <tr><td>Password</td><td><input type="password" name="senha1" id="senha1" maxlength="10" size="10"></td></tr>
+        <tr><td>Confirme a password</td><td><input type="password" name="senha2" id="senha2" maxlength="10" size="10"></td></tr>
+        <tr><td><input type="submit" value="Guardar"></td></tr>
+    </table>
+</form>
